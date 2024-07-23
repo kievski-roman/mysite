d0916227cd0ecdec0a1181de5c0eadddc81b4583
Kievski-Roman <rkievski@gmail.com>
2024-07-23 06:47:01 +0200

    Color coding for each digit in multiplication expression

---
+++ lesson2.php
@@ -0,0 +1,49 @@
+<?php
+include 'header.php';
+include 'nav.php';
+?>
+<main>
+    <h2>Розфарбована таблиця множення</h2>
+    <table border="1" cellspacing="0" cellpadding="5">
+        <?php
+        function getColor($digit) {
+            switch ($digit) {
+                case '1': return 'red';
+                case '2': return 'green';
+                case '3': return 'yellow';
+                case '4': return 'blue';
+                default: return 'black';
+            }
+        }
+
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<tr>";
+            for ($j = 1; $j <= 10; $j++) {
+                $product = $i * $j;
+                
+                // Разукрашивание чисел в выражении
+                $coloredI = '';
+                foreach (str_split(strval($i)) as $digit) {
+                    $coloredI .= "<span style='color: " . getColor($digit) . ";'>$digit</span>";
+                }
+
+                $coloredJ = '';
+                foreach (str_split(strval($j)) as $digit) {
+                    $coloredJ .= "<span style='color: " . getColor($digit) . ";'>$digit</span>";
+                }
+
+                $coloredProduct = '';
+                foreach (str_split(strval($product)) as $digit) {
+                    $coloredProduct .= "<span style='color: " . getColor($digit) . ";'>$digit</span>";
+                }
+
+                echo "<td>$coloredI x $coloredJ = $coloredProduct</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</main>
+<?php
+include 'footer.php';
+?>
