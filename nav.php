19a41282d1e6faee15a69038903ddb1119d58f80
Kievski-Roman <rkievski@gmail.com>
2024-07-23 06:56:35 +0200

    Updated nav.php and fixed undefined array key error in lesson 3

--- nav.php
+++ nav.php
@@ -2,5 +2,7 @@
     <ul>
         <li><a href="lesson1.php">Урок 1</a></li>
         <li><a href="lesson2.php">Урок 2</a></li>
+        <li><a href="lesson3.php">Урок 3</a></li>
     </ul>
 </nav>
+
