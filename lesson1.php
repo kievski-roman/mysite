1eee3c1791469955ddd506e01afd775d86844607
Kievski-Roman <rkievski@gmail.com>
2024-07-23 06:20:02 +0200

    Added formatted multiplication table in lesson 1

--- lesson1.php
+++ lesson1.php
@@ -3,19 +3,31 @@ include 'header.php';
 include 'nav.php';
 ?>
 <main>
-    <h2>Таблица умножения</h2>
+    <h2>Таблиця множення</h2>
     <table border="1">
         <?php
-        for ($i = 1; $i <= 10; $i++) {
-            echo "<tr>";
+        echo "<tr>";
+        for ($i = 1; $i <= 5; $i++) {
+            echo "<td>";
             for ($j = 1; $j <= 10; $j++) {
-                echo "<td>" . ($i * $j) . "</td>";
+                echo "$i x $j = " . ($i * $j) . "<br>";
             }
-            echo "</tr>";
+            echo "</td>";
         }
+        echo "</tr>";
+        echo "<tr>";
+        for ($i = 6; $i <= 10; $i++) {
+            echo "<td>";
+            for ($j = 1; $j <= 10; $j++) {
+                echo "$i x $j = " . ($i * $j) . "<br>";
+            }
+            echo "</td>";
+        }
+        echo "</tr>";
         ?>
     </table>
 </main>
 <?php
 include 'footer.php';
 ?>
+
